2c525af6d48d46be8fee19c3fc2ef06713273be5
Daniel Lewis <daniellewis777@gmail.com>
2018-03-03 21:36:51 +0000

    more code tidying

--- py/Report.py
+++ py/Report.py
@@ -9,10 +9,10 @@ class Report:
         self.version = version
 
     def processed(self, book_num, book_name, start):
-        elapsed = str(math.floor(time.time() - start)).zfill(3)
         str_book_num = str(book_num).zfill(2)
         str_book_name = book_name.ljust(6)
         bar = '|' + '#' * (book_num - 1) + ' ' * (66 - book_num) + '|'
+        elapsed = str(math.floor(time.time() - start)).zfill(3)
         msg = f'{self.version} - {str_book_num} {str_book_name} {bar} {elapsed} sec'
         sys.stdout.write('\r' + msg)
         sys.stdout.flush()
